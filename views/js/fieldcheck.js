834e074f6664ec0e777c0058222be59ba5bbf125
lea <blue.blood@gmx.de>
2019-12-06 23:42:00 +0000

    Password input check

--- views/js/fieldcheck.js
+++ views/js/fieldcheck.js
@@ -4,5 +4,23 @@ function initChecking() {
 
 function valueInteraction() {
     let passwordStatus = document.getElementById("registrationPassword");
-    let verifyStatus = document.getElementById("registrationVerifyPassword")
+    let verifyStatus = document.getElementById("registrationVerifyPassword");
+
+    passwordStatus.addEventListener("keyup", checkPasswords);
+    verifyStatus.addEventListener("keyup", checkPasswords);
+}
+
+function checkPasswords() {
+    let passwordStatus = document.getElementById("registrationPassword");
+    let verifyStatus = document.getElementById("registrationVerifyPassword");
+
+    if (passwordStatus.value !== verifyStatus.value){
+        passwordStatus.className = "form-control is-invalid";
+        verifyStatus.className = "form-control is-invalid";
+    }
+
+    else {
+        passwordStatus.className = "form-control is-valid";
+        verifyStatus.className = "form-control is-valid";
+    }
 }
