cb4aac3cae9e229d5fee833460579222eedd7330
lea <blue.blood@gmx.de>
2019-12-06 23:42:00 +0000

    First steps of input check in .js in browser

---
+++ views/js/fieldcheck.js
@@ -0,0 +1,8 @@
+function initChecking() {
+    valueInteraction();
+}
+
+function valueInteraction() {
+    let passwordStatus = document.getElementById("registrationPassword");
+    let verifyStatus = document.getElementById("registrationVerifyPassword")
+}
